c7fb8252ac3b223aa877d12a97b9540262985474
Diwakar Gyawali <076bct024.diwakar@pcampus.edu.np>
2022-08-24 09:31:47 +0545

    background added

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+unsigned int loadSkybox(vector<std::string> faces);
 
 // settings
 const unsigned int SCR_WIDTH = 1920;
@@ -78,17 +79,95 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
 
+	//load and compile shaders 
+
+    Shader ourShader("./shaders/1.model_loading.vs", "./shaders/1.model_loading.fs");
+	Shader skyboxShader("./shaders/2.skyBox.vs","./shaders/2.skyBox.fs");
+
+
+	//load models 
+
+	Model ourModel("./blenderneigh/neigh2.obj");
+
+
+	// skybox vertices 
+	float skyboxVertices[] = 
+	{
+    // positions          
+    -100.0f,  100.0f, -100.0f,
+    -100.0f, -100.0f, -100.0f,
+     100.0f, -100.0f, -100.0f,
+     100.0f, -100.0f, -100.0f,
+     100.0f,  100.0f, -100.0f,
+    -100.0f,  100.0f, -100.0f,
+
+    -100.0f, -100.0f,  100.0f,
+    -100.0f, -100.0f, -100.0f,
+    -100.0f,  100.0f, -100.0f,
+    -100.0f,  100.0f, -100.0f,
+    -100.0f,  100.0f,  100.0f,
+    -100.0f, -100.0f,  100.0f,
+
+     100.0f, -100.0f, -100.0f,
+     100.0f, -100.0f,  100.0f,
+     100.0f,  100.0f,  100.0f,
+     100.0f,  100.0f,  100.0f,
+     100.0f,  100.0f, -100.0f,
+     100.0f, -100.0f, -100.0f,
+
+    -100.0f, -100.0f,  100.0f,
+    -100.0f,  100.0f,  100.0f,
+     100.0f,  100.0f,  100.0f,
+     100.0f,  100.0f,  100.0f,
+     100.0f, -100.0f,  100.0f,
+    -100.0f, -100.0f,  100.0f,
+
+    -100.0f,  100.0f, -100.0f,
+     100.0f,  100.0f, -100.0f,
+     100.0f,  100.0f,  100.0f,
+     100.0f,  100.0f,  100.0f,
+    -100.0f,  100.0f,  100.0f,
+    -100.0f,  100.0f, -100.0f,
+
+    -100.0f, -100.0f, -100.0f,
+    -100.0f, -100.0f,  100.0f,
+     100.0f, -100.0f, -100.0f,
+     100.0f, -100.0f, -100.0f,
+    -100.0f, -100.0f,  100.0f,
+     100.0f, -100.0f,  100.0f
+	};
+//faces for skybox 
+	vector<std::string> faces
+	{
+    "./skybox/right.jpg",
+    "./skybox/left.jpg",
+    "./skybox/top.jpg",
+    "./skybox/bottom.jpg",
+    "./skybox/front.jpg",
+    "./skybox/back.jpg"
+	};
+
+ unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    unsigned int cubemapTexture = loadSkybox(faces);
+    skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
+
 
     // build and compile shaders
     // -------------------------
-    Shader ourShader("./shaders/1.model_loading.vs", "./shaders/1.model_loading.fs");
 	//Shader lightCubeShader("./shaders/2.lightCube.vs", "./shaders/2.lightCube.fs");
 
     // load models
     // -----------
     //Model ourModel("myfirstblender/myfirstblender.obj");
 	//Model ourModel("blend/neigh2 (1).obj");
-	Model ourModel("./blenderneigh/neigh2.obj");
 //	Model ourModel("./blenderneigh/diwakar.obj");
    //Model ourModel("objects/dharahara/dharaharaSeriousNewScenes.obj");
     
@@ -143,7 +222,19 @@ int main()
 
 
 
-		//render light cubes 
+		//render skybox 
+
+		glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        skyboxShader.use();
+        skyboxShader.setMat4("view", view);
+        skyboxShader.setMat4("projection", projection);
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
 
 		
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -216,4 +307,35 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+unsigned int loadSkybox(vector<std::string> faces)
+{
+    stbi_set_flip_vertically_on_load(false);
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
 }
